5760f44d4a08ab3603a4f527321695102ae8f284
1337968347 <1337968347@qq.com>
2021-08-01 15:47:07 +0800

    :star: MV

--- src/lib/MV.ts
+++ src/lib/MV.ts
@@ -1754,369 +1754,368 @@ export const quat4 = {
     dest[3] = quat[3];
     return dest;
   },
-};
+  /*
+   * quat4.length
+   * Calculates the length of a quat4
+   *
+   * Params:
+   * quat - quat4 to calculate length of
+   *
+   * Returns:
+   * Length of quat
+   */
+  length: function (quat) {
+    var x = quat[0],
+      y = quat[1],
+      z = quat[2],
+      w = quat[3];
+    return Math.sqrt(x * x + y * y + z * z + w * w);
+  },
 
-/*
- * quat4.length
- * Calculates the length of a quat4
- *
- * Params:
- * quat - quat4 to calculate length of
- *
- * Returns:
- * Length of quat
- */
-quat4.length = function (quat) {
-  var x = quat[0],
-    y = quat[1],
-    z = quat[2],
-    w = quat[3];
-  return Math.sqrt(x * x + y * y + z * z + w * w);
-};
+  /*
+   * quat4.normalize
+   * Generates a unit quaternion of the same direction as the provided quat4
+   * If quaternion length is 0, returns [0, 0, 0, 0]
+   *
+   * Params:
+   * quat - quat4 to normalize
+   * dest - Optional, quat4 receiving operation result. If not specified result is written to quat
+   *
+   * Returns:
+   * dest if specified, quat otherwise
+   */
+  normalize: function (quat, dest) {
+    if (!dest) {
+      dest = quat;
+    }
 
-/*
- * quat4.normalize
- * Generates a unit quaternion of the same direction as the provided quat4
- * If quaternion length is 0, returns [0, 0, 0, 0]
- *
- * Params:
- * quat - quat4 to normalize
- * dest - Optional, quat4 receiving operation result. If not specified result is written to quat
- *
- * Returns:
- * dest if specified, quat otherwise
- */
-quat4.normalize = function (quat, dest) {
-  if (!dest) {
-    dest = quat;
-  }
+    var x = quat[0],
+      y = quat[1],
+      z = quat[2],
+      w = quat[3];
+    var len = Math.sqrt(x * x + y * y + z * z + w * w);
+    if (len == 0) {
+      dest[0] = 0;
+      dest[1] = 0;
+      dest[2] = 0;
+      dest[3] = 0;
+      return dest;
+    }
+    len = 1 / len;
+    dest[0] = x * len;
+    dest[1] = y * len;
+    dest[2] = z * len;
+    dest[3] = w * len;
 
-  var x = quat[0],
-    y = quat[1],
-    z = quat[2],
-    w = quat[3];
-  var len = Math.sqrt(x * x + y * y + z * z + w * w);
-  if (len == 0) {
-    dest[0] = 0;
-    dest[1] = 0;
-    dest[2] = 0;
+    return dest;
+  },
+
+  /*
+   * quat4.multiply
+   * Performs a quaternion multiplication
+   *
+   * Params:
+   * quat - quat4, first operand
+   * quat2 - quat4, second operand
+   * dest - Optional, quat4 receiving operation result. If not specified result is written to quat
+   *
+   * Returns:
+   * dest if specified, quat otherwise
+   */
+  multiply: function (quat, quat2, dest) {
+    if (!dest) {
+      dest = quat;
+    }
+
+    var qax = quat[0],
+      qay = quat[1],
+      qaz = quat[2],
+      qaw = quat[3];
+    var qbx = quat2[0],
+      qby = quat2[1],
+      qbz = quat2[2],
+      qbw = quat2[3];
+
+    dest[0] = qax * qbw + qaw * qbx + qay * qbz - qaz * qby;
+    dest[1] = qay * qbw + qaw * qby + qaz * qbx - qax * qbz;
+    dest[2] = qaz * qbw + qaw * qbz + qax * qby - qay * qbx;
+    dest[3] = qaw * qbw - qax * qbx - qay * qby - qaz * qbz;
+
+    return dest;
+  },
+
+  /*
+   * quat4.multiplyVec3
+   * Transforms a vec3 with the given quaternion
+   *
+   * Params:
+   * quat - quat4 to transform the vector with
+   * vec - vec3 to transform
+   * dest - Optional, vec3 receiving operation result. If not specified result is written to vec
+   *
+   * Returns:
+   * dest if specified, vec otherwise
+   */
+  multiplyVec3: function (quat, vec, dest) {
+    if (!dest) {
+      dest = vec;
+    }
+
+    var x = vec[0],
+      y = vec[1],
+      z = vec[2];
+    var qx = quat[0],
+      qy = quat[1],
+      qz = quat[2],
+      qw = quat[3];
+
+    // calculate quat * vec
+    var ix = qw * x + qy * z - qz * y;
+    var iy = qw * y + qz * x - qx * z;
+    var iz = qw * z + qx * y - qy * x;
+    var iw = -qx * x - qy * y - qz * z;
+
+    // calculate result * inverse quat
+    dest[0] = ix * qw + iw * -qx + iy * -qz - iz * -qy;
+    dest[1] = iy * qw + iw * -qy + iz * -qx - ix * -qz;
+    dest[2] = iz * qw + iw * -qz + ix * -qy - iy * -qx;
+
+    return dest;
+  },
+
+  /*
+   * quat4.toMat3
+   * Calculates a 3x3 matrix from the given quat4
+   *
+   * Params:
+   * quat - quat4 to create matrix from
+   * dest - Optional, mat3 receiving operation result
+   *
+   * Returns:
+   * dest if specified, a new mat3 otherwise
+   */
+  toMat3: function (quat, dest) {
+    if (!dest) {
+      dest = mat3.create();
+    }
+
+    var x = quat[0],
+      y = quat[1],
+      z = quat[2],
+      w = quat[3];
+
+    var x2 = x + x;
+    var y2 = y + y;
+    var z2 = z + z;
+
+    var xx = x * x2;
+    var xy = x * y2;
+    var xz = x * z2;
+
+    var yy = y * y2;
+    var yz = y * z2;
+    var zz = z * z2;
+
+    var wx = w * x2;
+    var wy = w * y2;
+    var wz = w * z2;
+
+    dest[0] = 1 - (yy + zz);
+    dest[1] = xy - wz;
+    dest[2] = xz + wy;
+
+    dest[3] = xy + wz;
+    dest[4] = 1 - (xx + zz);
+    dest[5] = yz - wx;
+
+    dest[6] = xz - wy;
+    dest[7] = yz + wx;
+    dest[8] = 1 - (xx + yy);
+
+    return dest;
+  },
+
+  /*
+   * quat4.toMat4
+   * Calculates a 4x4 matrix from the given quat4
+   *
+   * Params:
+   * quat - quat4 to create matrix from
+   * dest - Optional, mat4 receiving operation result
+   *
+   * Returns:
+   * dest if specified, a new mat4 otherwise
+   */
+  toMat4: function (quat, dest) {
+    if (!dest) {
+      dest = mat4.create();
+    }
+
+    var x = quat[0],
+      y = quat[1],
+      z = quat[2],
+      w = quat[3];
+
+    var x2 = x + x;
+    var y2 = y + y;
+    var z2 = z + z;
+
+    var xx = x * x2;
+    var xy = x * y2;
+    var xz = x * z2;
+
+    var yy = y * y2;
+    var yz = y * z2;
+    var zz = z * z2;
+
+    var wx = w * x2;
+    var wy = w * y2;
+    var wz = w * z2;
+
+    dest[0] = 1 - (yy + zz);
+    dest[1] = xy - wz;
+    dest[2] = xz + wy;
     dest[3] = 0;
+
+    dest[4] = xy + wz;
+    dest[5] = 1 - (xx + zz);
+    dest[6] = yz - wx;
+    dest[7] = 0;
+
+    dest[8] = xz - wy;
+    dest[9] = yz + wx;
+    dest[10] = 1 - (xx + yy);
+    dest[11] = 0;
+
+    dest[12] = 0;
+    dest[13] = 0;
+    dest[14] = 0;
+    dest[15] = 1;
+
     return dest;
-  }
-  len = 1 / len;
-  dest[0] = x * len;
-  dest[1] = y * len;
-  dest[2] = z * len;
-  dest[3] = w * len;
+  },
 
-  return dest;
+  /*
+   * quat4.slerp
+   * Performs a spherical linear interpolation between two quat4
+   *
+   * Params:
+   * quat - quat4, first quaternion
+   * quat2 - quat4, second quaternion
+   * slerp - interpolation amount between the two inputs
+   * dest - Optional, quat4 receiving operation result. If not specified result is written to quat
+   *
+   * Returns:
+   * dest if specified, quat otherwise
+   */
+  slerp: function (quat, quat2, slerp, dest) {
+    if (!dest) {
+      dest = quat;
+    }
+
+    var cosHalfTheta = quat[0] * quat2[0] + quat[1] * quat2[1] + quat[2] * quat2[2] + quat[3] * quat2[3];
+
+    if (Math.abs(cosHalfTheta) >= 1.0) {
+      if (dest != quat) {
+        dest[0] = quat[0];
+        dest[1] = quat[1];
+        dest[2] = quat[2];
+        dest[3] = quat[3];
+      }
+      return dest;
+    }
+
+    var halfTheta = Math.acos(cosHalfTheta);
+    var sinHalfTheta = Math.sqrt(1.0 - cosHalfTheta * cosHalfTheta);
+
+    if (Math.abs(sinHalfTheta) < 0.001) {
+      dest[0] = quat[0] * 0.5 + quat2[0] * 0.5;
+      dest[1] = quat[1] * 0.5 + quat2[1] * 0.5;
+      dest[2] = quat[2] * 0.5 + quat2[2] * 0.5;
+      dest[3] = quat[3] * 0.5 + quat2[3] * 0.5;
+      return dest;
+    }
+
+    var ratioA = Math.sin((1 - slerp) * halfTheta) / sinHalfTheta;
+    var ratioB = Math.sin(slerp * halfTheta) / sinHalfTheta;
+
+    dest[0] = quat[0] * ratioA + quat2[0] * ratioB;
+    dest[1] = quat[1] * ratioA + quat2[1] * ratioB;
+    dest[2] = quat[2] * ratioA + quat2[2] * ratioB;
+    dest[3] = quat[3] * ratioA + quat2[3] * ratioB;
+
+    return dest;
+  },
+
+  /*
+   * quat4.str
+   * Returns a string representation of a quaternion
+   *
+   * Params:
+   * quat - quat4 to represent as a string
+   *
+   * Returns:
+   * string representation of quat
+   */
+  str: function (quat) {
+    return '[' + quat[0] + ', ' + quat[1] + ', ' + quat[2] + ', ' + quat[3] + ']';
+  },
 };
 
-/*
- * quat4.multiply
- * Performs a quaternion multiplication
- *
- * Params:
- * quat - quat4, first operand
- * quat2 - quat4, second operand
- * dest - Optional, quat4 receiving operation result. If not specified result is written to quat
- *
- * Returns:
- * dest if specified, quat otherwise
- */
-quat4.multiply = function (quat, quat2, dest) {
-  if (!dest) {
-    dest = quat;
-  }
+export const vec4 = {
+  /*
+   * vec4.create
+   * Creates a new instance of a vec3 using the default array type
+   * Any javascript array containing at least 3 numeric elements can serve as a vec3
+   *
+   * Params:
+   * vec - Optional, vec3 or 4 containing values to initialize with
+   * w - Optional w component used to initialize the vec4, required when a vec3 is provided
+   *
+   * Returns  :
+   * New vec4
+   */
+  create: function (vec, w) {
+    var dest = new Float32Array(4);
 
-  var qax = quat[0],
-    qay = quat[1],
-    qaz = quat[2],
-    qaw = quat[3];
-  var qbx = quat2[0],
-    qby = quat2[1],
-    qbz = quat2[2],
-    qbw = quat2[3];
-
-  dest[0] = qax * qbw + qaw * qbx + qay * qbz - qaz * qby;
-  dest[1] = qay * qbw + qaw * qby + qaz * qbx - qax * qbz;
-  dest[2] = qaz * qbw + qaw * qbz + qax * qby - qay * qbx;
-  dest[3] = qaw * qbw - qax * qbx - qay * qby - qaz * qbz;
-
-  return dest;
-};
-
-/*
- * quat4.multiplyVec3
- * Transforms a vec3 with the given quaternion
- *
- * Params:
- * quat - quat4 to transform the vector with
- * vec - vec3 to transform
- * dest - Optional, vec3 receiving operation result. If not specified result is written to vec
- *
- * Returns:
- * dest if specified, vec otherwise
- */
-quat4.multiplyVec3 = function (quat, vec, dest) {
-  if (!dest) {
-    dest = vec;
-  }
-
-  var x = vec[0],
-    y = vec[1],
-    z = vec[2];
-  var qx = quat[0],
-    qy = quat[1],
-    qz = quat[2],
-    qw = quat[3];
-
-  // calculate quat * vec
-  var ix = qw * x + qy * z - qz * y;
-  var iy = qw * y + qz * x - qx * z;
-  var iz = qw * z + qx * y - qy * x;
-  var iw = -qx * x - qy * y - qz * z;
-
-  // calculate result * inverse quat
-  dest[0] = ix * qw + iw * -qx + iy * -qz - iz * -qy;
-  dest[1] = iy * qw + iw * -qy + iz * -qx - ix * -qz;
-  dest[2] = iz * qw + iw * -qz + ix * -qy - iy * -qx;
-
-  return dest;
-};
-
-/*
- * quat4.toMat3
- * Calculates a 3x3 matrix from the given quat4
- *
- * Params:
- * quat - quat4 to create matrix from
- * dest - Optional, mat3 receiving operation result
- *
- * Returns:
- * dest if specified, a new mat3 otherwise
- */
-quat4.toMat3 = function (quat, dest) {
-  if (!dest) {
-    dest = mat3.create();
-  }
-
-  var x = quat[0],
-    y = quat[1],
-    z = quat[2],
-    w = quat[3];
-
-  var x2 = x + x;
-  var y2 = y + y;
-  var z2 = z + z;
-
-  var xx = x * x2;
-  var xy = x * y2;
-  var xz = x * z2;
-
-  var yy = y * y2;
-  var yz = y * z2;
-  var zz = z * z2;
-
-  var wx = w * x2;
-  var wy = w * y2;
-  var wz = w * z2;
-
-  dest[0] = 1 - (yy + zz);
-  dest[1] = xy - wz;
-  dest[2] = xz + wy;
-
-  dest[3] = xy + wz;
-  dest[4] = 1 - (xx + zz);
-  dest[5] = yz - wx;
-
-  dest[6] = xz - wy;
-  dest[7] = yz + wx;
-  dest[8] = 1 - (xx + yy);
-
-  return dest;
-};
-
-/*
- * quat4.toMat4
- * Calculates a 4x4 matrix from the given quat4
- *
- * Params:
- * quat - quat4 to create matrix from
- * dest - Optional, mat4 receiving operation result
- *
- * Returns:
- * dest if specified, a new mat4 otherwise
- */
-quat4.toMat4 = function (quat, dest) {
-  if (!dest) {
-    dest = mat4.create();
-  }
-
-  var x = quat[0],
-    y = quat[1],
-    z = quat[2],
-    w = quat[3];
-
-  var x2 = x + x;
-  var y2 = y + y;
-  var z2 = z + z;
-
-  var xx = x * x2;
-  var xy = x * y2;
-  var xz = x * z2;
-
-  var yy = y * y2;
-  var yz = y * z2;
-  var zz = z * z2;
-
-  var wx = w * x2;
-  var wy = w * y2;
-  var wz = w * z2;
-
-  dest[0] = 1 - (yy + zz);
-  dest[1] = xy - wz;
-  dest[2] = xz + wy;
-  dest[3] = 0;
-
-  dest[4] = xy + wz;
-  dest[5] = 1 - (xx + zz);
-  dest[6] = yz - wx;
-  dest[7] = 0;
-
-  dest[8] = xz - wy;
-  dest[9] = yz + wx;
-  dest[10] = 1 - (xx + yy);
-  dest[11] = 0;
-
-  dest[12] = 0;
-  dest[13] = 0;
-  dest[14] = 0;
-  dest[15] = 1;
-
-  return dest;
-};
-
-/*
- * quat4.slerp
- * Performs a spherical linear interpolation between two quat4
- *
- * Params:
- * quat - quat4, first quaternion
- * quat2 - quat4, second quaternion
- * slerp - interpolation amount between the two inputs
- * dest - Optional, quat4 receiving operation result. If not specified result is written to quat
- *
- * Returns:
- * dest if specified, quat otherwise
- */
-quat4.slerp = function (quat, quat2, slerp, dest) {
-  if (!dest) {
-    dest = quat;
-  }
-
-  var cosHalfTheta = quat[0] * quat2[0] + quat[1] * quat2[1] + quat[2] * quat2[2] + quat[3] * quat2[3];
-
-  if (Math.abs(cosHalfTheta) >= 1.0) {
-    if (dest != quat) {
-      dest[0] = quat[0];
-      dest[1] = quat[1];
-      dest[2] = quat[2];
-      dest[3] = quat[3];
+    if (vec) {
+      dest[0] = vec[0];
+      dest[1] = vec[1];
+      dest[2] = vec[2];
+      if (vec.length > 3) {
+        dest[3] = vec[3];
+      } else {
+        dest[3] = w;
+      }
     }
     return dest;
-  }
+  },
 
-  var halfTheta = Math.acos(cosHalfTheta);
-  var sinHalfTheta = Math.sqrt(1.0 - cosHalfTheta * cosHalfTheta);
-
-  if (Math.abs(sinHalfTheta) < 0.001) {
-    dest[0] = quat[0] * 0.5 + quat2[0] * 0.5;
-    dest[1] = quat[1] * 0.5 + quat2[1] * 0.5;
-    dest[2] = quat[2] * 0.5 + quat2[2] * 0.5;
-    dest[3] = quat[3] * 0.5 + quat2[3] * 0.5;
-    return dest;
-  }
-
-  var ratioA = Math.sin((1 - slerp) * halfTheta) / sinHalfTheta;
-  var ratioB = Math.sin(slerp * halfTheta) / sinHalfTheta;
-
-  dest[0] = quat[0] * ratioA + quat2[0] * ratioB;
-  dest[1] = quat[1] * ratioA + quat2[1] * ratioB;
-  dest[2] = quat[2] * ratioA + quat2[2] * ratioB;
-  dest[3] = quat[3] * ratioA + quat2[3] * ratioB;
-
-  return dest;
-};
-
-/*
- * quat4.str
- * Returns a string representation of a quaternion
- *
- * Params:
- * quat - quat4 to represent as a string
- *
- * Returns:
- * string representation of quat
- */
-quat4.str = function (quat) {
-  return '[' + quat[0] + ', ' + quat[1] + ', ' + quat[2] + ', ' + quat[3] + ']';
-};
-
-vec4 = {};
-
-/*
- * vec4.create
- * Creates a new instance of a vec3 using the default array type
- * Any javascript array containing at least 3 numeric elements can serve as a vec3
- *
- * Params:
- * vec - Optional, vec3 or 4 containing values to initialize with
- * w - Optional w component used to initialize the vec4, required when a vec3 is provided
- *
- * Returns  :
- * New vec4
- */
-vec4.create = function (vec, w) {
-  var dest = new Float32Array(4);
-
-  if (vec) {
-    dest[0] = vec[0];
-    dest[1] = vec[1];
-    dest[2] = vec[2];
-    if (vec.length > 3) {
-      dest[3] = vec[3];
-    } else {
-      dest[3] = w;
+  /*
+   * vec4.scale
+   * Multiplies the components of a vec4 by a scalar value
+   *
+   * Params:
+   * vec - vec4 to scale
+   * val - Numeric value to scale by
+   * dest - Optional, vec4 receiving operation result. If not specified result is written to vec
+   *
+   * Returns:
+   * dest if specified, vec otherwise
+   */
+  scale: function (vec, val, dest) {
+    if (!dest || vec == dest) {
+      vec[0] *= val;
+      vec[1] *= val;
+      vec[2] *= val;
+      vec[3] *= val;
+      return vec;
     }
-  }
-  return dest;
-};
 
-/*
- * vec4.scale
- * Multiplies the components of a vec4 by a scalar value
- *
- * Params:
- * vec - vec4 to scale
- * val - Numeric value to scale by
- * dest - Optional, vec4 receiving operation result. If not specified result is written to vec
- *
- * Returns:
- * dest if specified, vec otherwise
- */
-vec4.scale = function (vec, val, dest) {
-  if (!dest || vec == dest) {
-    vec[0] *= val;
-    vec[1] *= val;
-    vec[2] *= val;
-    vec[3] *= val;
-    return vec;
-  }
-
-  dest[0] = vec[0] * val;
-  dest[1] = vec[1] * val;
-  dest[2] = vec[2] * val;
-  dest[3] = vec[3] * val;
-  return dest;
+    dest[0] = vec[0] * val;
+    dest[1] = vec[1] * val;
+    dest[2] = vec[2] * val;
+    dest[3] = vec[3] * val;
+    return dest;
+  },
 };
